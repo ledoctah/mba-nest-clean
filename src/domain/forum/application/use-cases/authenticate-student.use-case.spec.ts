6b647ed9e83a4eb446509662b9442d6aee1b37e2
Matheus <matheus@matheusferreira.dev>
2024-11-19 22:27:38 -0300

    tests: add testes to authenticate and register student

---
+++ src/domain/forum/application/use-cases/authenticate-student.use-case.spec.ts
@@ -0,0 +1,44 @@
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter';
+import { FakeHasher } from 'test/cryptography/fake-hasher';
+import { makeStudent } from 'test/factories/make-student';
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository';
+
+import { AuthenticateStudentUseCase } from './authenticate-student.use-case';
+
+let sut: AuthenticateStudentUseCase;
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
+let fakeHasher: FakeHasher;
+let fakeEncrypter: FakeEncrypter;
+
+describe('Authenticate Student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
+    fakeHasher = new FakeHasher();
+    fakeEncrypter = new FakeEncrypter();
+
+    sut = new AuthenticateStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    );
+  });
+
+  it('should be able to authenticate student', async () => {
+    const student = makeStudent({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    });
+
+    inMemoryStudentsRepository.items.push(student);
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    });
+  });
+});
